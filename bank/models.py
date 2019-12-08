7fd2fb63c224a21f4ccdbc2fdba6da0a1edee5de
nikitanovozhilkin <nikitanovozhilkin261@gmail.com>
2019-12-08 16:58:19 +0300

    reduce mistakes in models.py

--- bank/models.py
+++ bank/models.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse
+from django.utils import timezone
 
 class User(AbstractUser):
     sourname = models.CharField(max_length=30, blank=True)
@@ -8,9 +12,9 @@ class User(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
     telephone_number = models.CharField(max_length=30, blank=True)
 
-class Deposit(models.model):
+class Deposit(models.Model):
     number= models.CharField(max_length=30, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.Cascade)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
@@ -22,7 +26,7 @@ class Deposit(models.model):
 class CreditDeposit(Deposit):
     percent= models.PositiveIntegerField()
     balance = models.PositiveIntegerField()
-    dolg= balance + balance * (percent / 100)
+
 
 
 class DebitDeposit(Deposit):
@@ -30,11 +34,15 @@ class DebitDeposit(Deposit):
 
 
 
-class Card (models.model):
+class Card (models.Model):
      number= models.CharField(max_length=30, blank=True)
-     deposit = models.ForeignKey(Bill, on_delete=models.Cascade)
+     deposit = models.ForeignKey(Deposit, on_delete=models.CASCADE)
 
-class Application(models.model):
+
+
+
+
+class Application(models.Model):
     number= models.CharField(max_length=30, blank=True)
     sourname = models.CharField(max_length=30, blank=True)
     name = models.CharField(max_length=30, blank=True)
@@ -42,6 +50,9 @@ class Application(models.model):
     birth_date = models.DateField(null=True, blank=True)
     telephone_number = models.CharField(max_length=30, blank=True)
     approved = models.BooleanField(default=False)
+    def approve(self):
+        self.approved = True
+        self.save()
 
 
 class Transaction(models.Model):
@@ -50,18 +61,8 @@ class Transaction(models.Model):
         on_delete=models.CASCADE,
     )
     timestamp = models.DateTimeField(auto_now_add=True)
-    deposit = models.ForeignKey(Deposit, on_delete=models.Cascade)
-    balance = models.ForeignKey(Deposit, on_delete=models.Cascade)
-
+    amount = models.PositiveIntegerField()
     def __str__(self):
         return str(self.user)
     class Meta:
         abstract = True
-class IncomeTransaction (Transaction):
-    amount= models.PositiveIntegerField()
-    balance =+ amount
-
-class OutcomeTransaction (Transaction):
-    amount= models.PositiveIntegerField()
-    balance =- amount
-
